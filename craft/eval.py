dc87b52e48fb7c5969d9f6d1fbd276476e1fc5d6
TranafLee <tranthao2492002@gmail.com>
2023-06-20 09:52:17 +0700

    change test folder name in visualization mode

--- craft/eval.py
+++ craft/eval.py
@@ -312,8 +312,9 @@ def main_eval(model_path, backbone, config, evaluator, result_dir, buffer, model
                 gt_box=total_imgs_bboxes_gt[k],
                 img_name=img_path,
                 result_dir=result_dir,
-                test_folder_name="./output_custom_data",
+                test_folder_name="custom_data",
             )
+            print("visualized!")
 
     # When distributed evaluation mode, wait until buffer is full filled
     if buffer is not None:

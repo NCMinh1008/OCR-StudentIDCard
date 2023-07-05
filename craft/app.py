0511477e776f26b277c07a2178b0159fcf0f42e2
TranafLee <tranthao2492002@gmail.com>
2023-07-05 15:32:26 +0700

    check bounds

--- craft/app.py
+++ craft/app.py
@@ -13,48 +13,49 @@ from config.load_config import load_yaml, DotDict
 def draw_boxes(image, bounds, color='yellow', width=2):
     draw = ImageDraw.Draw(image)
     for bound in bounds:
+        print(bound)
         p0, p1, p2, p3 = bound[0]
         draw.line([*p0, *p1, *p2, *p3, *p0], fill=color, width=width)
     return image
 
 def inference(img_path, lang):
     # ============================
-    parser = argparse.ArgumentParser(description="CRAFT Text Detection Eval")
-    parser.add_argument(
-        "--yaml",
-        "--yaml_file_name",
-        default="custom_data_train",
-        type=str,
-        help="Load configuration",
-    )
+    # parser = argparse.ArgumentParser(description="CRAFT Text Detection Eval")
+    # parser.add_argument(
+    #     "--yaml",
+    #     "--yaml_file_name",
+    #     default="custom_data_train",
+    #     type=str,
+    #     help="Load configuration",
+    # )
 
-    args = parser.parse_args()
+    # args = parser.parse_args()
 
-    # load configure
-    config = load_yaml(args.yaml)
-    config = DotDict(config)
+    # # load configure
+    # config = load_yaml(args.yaml)
+    # config = DotDict(config)
 
 
-    val_result_dir_name = args.yaml
-    total_imgs_bboxes_pre = cal_eval(
-        img_path,
-        config,
-        "custom_data",
-        val_result_dir_name + "-ic15-iou",
-        opt="iou_eval",
-        mode=None,
-    )
+    # val_result_dir_name = args.yaml
+    # total_imgs_bboxes_pre = cal_eval(
+    #     img_path,
+    #     config,
+    #     "custom_data",
+    #     val_result_dir_name + "-ic15-iou",
+    #     opt="iou_eval",
+    #     mode=None,
+    # )
 
-    bounds = []
-    for idx in range(len(total_imgs_bboxes_pre[0][0])):
-        bound = total_imgs_bboxes_pre[0][idx]["points"].tolist()
-        bounds.append(bound)
-        print(bound)
+    # bounds = []
+    # for idx in range(len(total_imgs_bboxes_pre[0][0])):
+    #     bound = total_imgs_bboxes_pre[0][idx]["points"].tolist()
+    #     bounds.append(bound)
+    #     print(bound)
     
 
     # ============================
     reader = easyocr.Reader(lang)
-    # bounds = reader.readtext(img_path)
+    bounds = reader.readtext(img_path)
     # print(bounds)
     im = PIL.Image.open(img_path)
     draw_boxes(im, bounds)
